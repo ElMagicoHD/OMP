4baa197c4ce7574e297e9a75d50a7f1c91878659
Alexander <48961123+ElMagicoHD@users.noreply.github.com>
2021-09-07 18:00:34 +0200

    adding some images

--- src/bench_viz.py
+++ src/bench_viz.py
@@ -1,9 +1,7 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import osmnx as ox
-import numpy as np
-import os
+import pandas as pd
+import seaborn as sns
 
 
 def duration_plotting(type_of_graph: str):
@@ -111,51 +109,78 @@ def difference_greedy(with_normal_greedy=True):
     )
     g.set(yscale="linear",
           xlabel="Anzahl der Knoten", ylabel="Dauer in Sekunden"
-    )
+          )
     plt.legend(loc="upper left")
     g.figure.savefig(title, bbox_inches="tight")
     plt.show()
     return
 
-def plot_city(name="meran"):
 
-    df = pd.DataFrame(pd.read_csv(filepath_or_buffer="../benchmarks/benchmarking_" + name + ".txt"))
-    dfb = df[["size_of_Q", "duration_baseline"]]
-    dfg = df[["size_of_Q", "duration_greedy"]]
-    dfb = dfb.rename(columns={"duration_baseline": "duration"})
-    dfg = dfg.rename(columns={"duration_greedy": "duration"})
-    dfb["Algorithm"] = "Baseline"
-    dfg["Algorithm"] = "Greedy"
-    df = pd.concat([dfb, dfg])
-    sns.set_context(context="talk")
-    g = sns.lineplot(
-        data=df, legend=True,
-        x="size_of_Q", y="duration",
-        hue="Algorithm",
-        ci="sd"
-    )
-    g.set(yscale="linear",
-          xlabel="|Q|", ylabel="Dauer in Sekunden"
-          )
-    plt.legend(loc="upper left")
-    g.figure.savefig("../images/meran_q.png", bbox_inches="tight")
-    plt.show()
+def plot_city(name="meran"):
+    if name == "meran":
+        df = pd.DataFrame(pd.read_csv(filepath_or_buffer="../benchmarks/benchmarking_" + name + ".txt"))
+        dfb = df[["size_of_Q", "duration_baseline"]]
+        dfg = df[["size_of_Q", "duration_greedy"]]
+        dfb = dfb.rename(columns={"duration_baseline": "duration"})
+        dfg = dfg.rename(columns={"duration_greedy": "duration"})
+        dfb["Algorithm"] = "Baseline"
+        dfg["Algorithm"] = "Greedy"
+        df = pd.concat([dfb, dfg])
+        sns.set_context(context="talk")
+        g = sns.lineplot(
+            data=df, legend=True,
+            x="size_of_Q", y="duration",
+            hue="Algorithm",
+            ci="sd"
+        )
+        g.set(yscale="linear",
+              xlabel="|Q|", ylabel="Dauer in Sekunden"
+              )
+        g.set_xticks([2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+        plt.legend(loc="upper left")
+        g.figure.savefig("../images/meran_q.png", bbox_inches="tight")
+        plt.show()
+    else:
+        df = pd.DataFrame(pd.read_csv(filepath_or_buffer="../benchmarks/benchmarking_" + name + ".txt"))
+        dfb = df[["size_of_Q", "duration_baseline"]]
+        dfg = df[["size_of_Q", "duration_greedy"]]
+        dfg2 = pd.DataFrame(pd.read_csv(filepath_or_buffer="../benchmarks/benchmarking_greedy_" + name + ".txt"))[
+            ["size_of_Q", "duration_greedy"]]
+        dfg = pd.concat([dfg, dfg2])
+        dfb["Algorithm"] = "Baseline"
+        dfg["Algorithm"] = "Greedy"
+        dfb = dfb.rename(columns={"duration_baseline": "duration"})
+        dfg = dfg.rename(columns={"duration_greedy": "duration"})
+        df = pd.concat([dfb, dfg])
+        df["duration"] = df["duration"] / 60.0
+        sns.set_context(context="talk")
+        g = sns.lineplot(
+            data=df, legend=True,
+            x="size_of_Q", y="duration",
+            hue="Algorithm",
+            ci="sd"
+        )
+        g.set(yscale="linear",
+              xlabel="|Q|", ylabel="Dauer in Minuten"
+              )
+        g.set_xticks([2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+        g.figure.savefig("../images/" + name + "_q_min.png", bbox_inches="tight")
+        plt.show()
 
 def streetnetwork(name="meran"):
-    fname = "/home/elmagico/OPM/data/"+ name + ".gxl"
+    fname = "/home/elmagico/OPM/data/" + name + ".gxl"
     Gx = ox.load_graphml(filepath=fname)
     # clist = ox.plot.get_colors(n=2, cmap="plasma", return_hex=True)
-    fig, ax = ox.plot.plot_graph(Gx, bgcolor="white", node_color="black", figsize=[8,8] )
-    fig.savefig("/home/elmagico/OPM/images/" + name + "_streetnetwork.png") 
+    fig, ax = ox.plot.plot_graph(Gx, bgcolor="white", node_color="black", figsize=[8, 8])
+    fig.savefig("/home/elmagico/OPM/images/" + name + "_streetnetwork.png")
 
 
 if __name__ == "__main__":
     # duration_plotting(type_of_graph="grid")
     # difference_greedy()
-    #difference_plotting(type_of_graph="random_02")
-    # plot_city()
-    streetnetwork("berlin")
-    streetnetwork("nyc")
-    streetnetwork("vienna")
-    streetnetwork("tokyo")
-
+    # difference_plotting(type_of_graph="random_02")
+    plot_city("")
+    # streetnetwork("berlin")
+    # streetnetwork("nyc")
+    # streetnetwork("vienna")
+    # streetnetwork("tokyo")
--- src/better_data.py
+++ src/better_data.py
@@ -1,17 +1,17 @@
+import ast
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import ast
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
 def better_data():
-
     # first 50 entries
-    data = pd.read_csv(filepath_or_buffer="../benchmarks/benchmarking_edges_per_node_random_05_1.txt", sep=",", nrows=50, skiprows=50, header=None)
+    data = pd.read_csv(filepath_or_buffer="../benchmarks/benchmarking_edges_per_node_random_05_1.txt", sep=",",
+                       nrows=50, skiprows=50, header=None)
 
-    #converting string to tuples
+    # converting string to tuples
     for i in data:
         data[i] = data[i].apply(ast.literal_eval)
 
@@ -24,7 +24,7 @@ def better_data():
     unique, counts = np.unique(flat, return_counts=True)    #help from https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray
     d = dict(zip(unique, counts))
     percentage = list(d.values())
-    percentage = [(i/sum(percentage))*100.0 for i in percentage]
+    percentage = [(i / sum(percentage)) * 100.0 for i in percentage]
     print(sum(percentage))
     sns.set_context(context="notebook")
     ax = sns.barplot(x=list(d.keys()), y=percentage, color="cornflowerblue")
@@ -33,10 +33,24 @@ def better_data():
     plt.show()
 
 
-
-
-
+def hist():
+    meran = [0, 138, 36, 495, 63, 5]
+    nyc = [0, 3766, 347, 26354, 24216, 601, 57, 3, 1]
+    tokyo = [0, 33057, 1207, 187071, 50343, 1042, 118, 14]
+    vienna = [0, 1926, 483, 9589, 3888, 150, 16, 1]
+    berlin = [0, 3308, 425, 16716, 7227, 178, 8]
+    l = np.array([])
+    for i in range(len(tokyo)):
+        x = np.repeat(i, tokyo[i])
+        l = np.append(l, x)
+    # print(np.median(l))
+    sns.set_context(context="talk")
+    g = sns.barplot(x=list(range(len(vienna))), y=vienna)
+    g.set(xlabel="Anzahl der Kanten pro Knoten", ylabel="Absolute Häufigkeit")
+    g.figure.savefig("../images/vienna_distribution.png", bbox_inches="tight")
+    plt.show()
 
 
 if __name__ == "__main__":
-    better_data()
+    # better_data()
+    hist()
