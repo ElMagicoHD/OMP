ef663167af2e66119235f7291bb0dd4673d6b971
Alexander <48961123+ElMagicoHD@users.noreply.github.com>
2021-11-18 02:42:01 +0100

    Deleted commented commands

--- src/main.py
+++ src/main.py
@@ -47,15 +47,6 @@ def city(name, size_of_Q):
             if 'graph edgedefault="directed"' in line:
                 data[i] = data[i].replace('graph edgedefault="directed"', 'graph edgedefault="undirected"')
                 break
-        # weight_index = data.index("  <key id=\"d15\" for=\"edge\" attr.name=\"length\" attr.type=\"string\" />\n")
-        # x_index = data.index("  <key id=\"d5\" for=\"node\" attr.name=\"x\" attr.type=\"string\" />\n")
-        # y_index = data.index("  <key id=\"d4\" for=\"node\" attr.name=\"y\" attr.type=\"string\" />\n")
-        # undirected_index = data.index('  <graph edgedefault=\"directed\">\n')
-        #
-        # data[weight_index] = "  <key id=\"d15\" for=\"edge\" attr.name=\"weight\" attr.type=\"float\" />\n"
-        # data[x_index] = "  <key id=\"d5\" for=\"node\" attr.name=\"x\" attr.type=\"float\" />\n"
-        # data[y_index] = "  <key id=\"d4\" for=\"node\" attr.name=\"y\" attr.type=\"float\" />\n"
-        # data[undirected_index] = "  <graph edgedefault=\"undirected\"\n>"
 
         with open("../data/" + name + "_nx.graphml", 'w') as file:
             file.writelines(data)
